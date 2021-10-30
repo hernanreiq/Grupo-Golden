70f2cf1367d295b3421258e8190a521ece5572ba
Reiq.Laptop <reiqvideos@gmail.com>
2021-10-30 09:33:14 -0400

    Información sobre nosotros totalmente responsive y estilizada

--- src/views/partials/information.jsx
+++ src/views/partials/information.jsx
@@ -3,26 +3,26 @@ import photoInformation from "../../img/Sobre Nosotros.png";
 
 export function Information() {
     return (
-        <div class="sobrenosotros">
-            <div class="fotosobrenosotros">
-                <img src={photoInformation} />
+        <section className="information">
+            <div className="title title-information">
+                <img src={photoInformation} alt="title information" />
             </div>
-            <div class="sobrenosotrosdescripcion">
-                <p>
-                    Grupo Golden nace de la necesidad de querer emprender, para de esta manera poder crecer a nivel ético y
-                    profesional.
-
-                    <br /><br />
-
-                    Nosotros somos un grupo de amigos que por situaciones de la vida, coincidimos al elegir la misma carrera
-                    universitaria de Ingeniería en Sistemas y Computación.
-
-                    <br /><br />
-
-                    Es por ello que estando en los tramos finales de la carrera, decidimos unirnos para motivarnos a
-                    intentar emprender nuevas propuestas.
-                </p>
+            <div className="text-justify py-5 h5 px-5">
+                <div className="container">
+                    <p>
+                        Grupo Golden nace de la necesidad de querer emprender, para de esta manera poder crecer a nivel ético y
+                        profesional.
+                    </p>
+                    <p>
+                        Nosotros somos un grupo de amigos que por situaciones de la vida, coincidimos al elegir la misma carrera
+                        universitaria de Ingeniería en Sistemas y Computación.
+                    </p>
+                    <p>
+                        Es por ello que estando en los tramos finales de la carrera, decidimos unirnos para motivarnos a
+                        intentar emprender nuevas propuestas.
+                    </p>
+                </div>
             </div>
-        </div>
+        </section>
     )
 }
